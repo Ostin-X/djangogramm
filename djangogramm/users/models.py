270fc86f056477f213e35f1cc8acde3c1213537d
foxstudent102416 <andrewpeshekhonov@gmail.com>
2022-08-18 18:51:30 +0300

    init

--- djangogramm/users/models.py
+++ djangogramm/users/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     bio = models.CharField(max_length=1000)
-    avatar = models.ImageField()
+    avatar = models.ImageField(null=True, blank=True)
 
     def __repr__(self):
         return f'<User {self.id}>'
