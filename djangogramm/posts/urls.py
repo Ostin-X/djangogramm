a41f86ec4ef1711f623af8efb3f63c60324cdc33
foxstudent102416 <andrewpeshekhonov@gmail.com>
2022-08-19 12:28:21 +0300

    init

--- djangogramm/posts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('<int:post_id>/', posts_here),
-    path('', posts_here),
-]
--- djangogramm/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('<int:user_id>/', users_here, name = 'user_id_name'),
-    path('<str:user_name>/', users_here, name = 'user_name_name'),
-    path('', users_here),
-]
