0eb6a1181d4a6afb977c15b5fcdadaf3879e253c
foxstudent102416 <andrewpeshekhonov@gmail.com>
2022-08-24 11:11:16 +0300

    something

--- djangogramm/settings.py
+++ djangogramm/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = 'djangogramm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'djangogramm',
-#         'USER': 'postgres',
-#         'PASSWORD': 'qwe',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogramm',
+        'USER': 'postgres',
+        'PASSWORD': 'qwe',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 # import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=600)
 # DATABASES['default'].update(db_from_env)
