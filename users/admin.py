ec18a2e6a4932e24c39c69926003290663ab3592
foxstudent102416 <andrewpeshekhonov@gmail.com>
2022-08-26 13:48:18 +0300

    some views

--- users/admin.py
+++ users/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 
 from .models import User
 
+
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'email', 'password', 'avatar')
     list_display_links = ('id', 'name')
     search_fields = ('name', 'bio')
+    list_editable = ('password',)
+    list_filter = ('name',)
+
 
 admin.site.register(User, UserAdmin)
