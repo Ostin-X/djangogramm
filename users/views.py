70f06424c7c4649d5ad48c0ecb15087871acf47b
foxstudent102416 <andrewpeshekhonov@gmail.com>
2022-09-11 21:50:01 +0300

    slug

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -12,12 +13,17 @@ from .models import User, Profile
 from djangogramm.utils import DataMixin, NotLoggedAllow
 
 
-class PasswordChangeCustomView(PasswordChangeView):
+class PasswordChangeCustomView(DataMixin, PasswordChangeView):
     form_class = PasswordChangeCustomForm
     template_name = 'registration/change_password.html'
 
     def get_success_url(self):
         return reverse_lazy('password_success', kwargs={'pk': self.kwargs['pk']})
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PasswordChangeCustomView, self).get_context_data(**kwargs)
+        c_def = self.get_user_context(title=f"Зміна пароля")
+        return {**context, **c_def}
 
 
 def password_success(request, *args, **kwargs):
@@ -32,7 +38,7 @@ class UserListView(DataMixin, ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(UserListView, self).get_context_data(**kwargs)
         c_def = self.get_user_context(title='Користувачі')
-        return dict(list(context.items()) + list(c_def.items()))
+        return {**context, **c_def}
 
     def get_queryset(self):
         return User.objects.filter(profile__is_invisible=False).order_by('-pk')
@@ -44,13 +50,12 @@ class UserDetailView(LoginRequiredMixin, DataMixin, DetailView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(UserDetailView, self).get_context_data(**kwargs)
         c_def = self.get_user_context(title=context['user'])
-        return dict(list(context.items()) + list(c_def.items()))
+        return {**context, **c_def}
 
 
 class UserUpdateView(LoginRequiredMixin, DataMixin, UpdateView):
     model = User
     form_class = UserForm
-
     second_form_class = ProfileForm
 
     def get_queryset(self, *args, **kwargs):
@@ -62,60 +67,16 @@ class UserUpdateView(LoginRequiredMixin, DataMixin, UpdateView):
         context = super(UserUpdateView, self).get_context_data(**kwargs)
         form2 = ProfileForm(self.request.POST or None, instance=self.request.user.profile)
         c_def = self.get_user_context(title=f"Редагування профілю {context['user']}", form2=form2)
-        # if 'form' not in kwargs:
-        #     kwargs['form'] = self.form_class(instance=self.get_object())
-        # if 'form2' not in kwargs:
-        #     kwargs['form2'] = self.second_form_class(instance=self.get_object().profile)
-
-        return dict(list(context.items()) + list(c_def.items()))
-        # return super(UserUpdateView, self).get_context_data(**kwargs)
+        return {**context, **c_def}
 
     def get_success_url(self):
         return self.object.profile.get_absolute_url()
 
     def post(self, request, *args, **kwargs):
-        # self.object = self.get_object()
-        form = self.form_class(request.POST, instance=request.user)
-        form2 = self.second_form_class(request.POST, instance=request.user.profile)
-        print(form.fields)
-        print(form2.fields)
+        form2 = self.second_form_class(request.POST, request.FILES, instance=request.user.profile)
         if form2.is_valid():
-            form2.save()
+            obj = form2.save()
         return super().post(request, *args, **kwargs)
-
-
-# class ProfileUpdateView(LoginRequiredMixin, DataMixin, UpdateView):
-#     model = User
-#     form_class = ProfileForm
-#
-#     second_form_class = UserForm
-#
-#     def get_queryset(self, *args, **kwargs):
-#         return super().get_queryset().filter(
-#             pk=self.request.user.pk
-#         )
-#
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super(ProfileUpdateView, self).get_context_data(**kwargs)
-#         form2 = UserForm(self.request.POST or None, instance=self.request.user)
-#         c_def = self.get_user_context(title=f"Редагування профілю {context['user']}", form2=form2)
-#         # if 'form' not in kwargs:
-#         #     kwargs['form'] = self.form_class(instance=self.get_object())
-#         # if 'form2' not in kwargs:
-#         #     kwargs['form2'] = self.second_form_class(instance=self.get_object().profile)
-#
-#         return dict(list(context.items()) + list(c_def.items()))
-#         # return super(UserUpdateView, self).get_context_data(**kwargs)
-#
-#     def get_success_url(self):
-#         return self.object.profile.get_absolute_url()
-#
-#     def post(self, request, *args, **kwargs):
-#         # self.object = self.get_object()
-#         form2 = self.second_form_class(request.POST, instance=request.user)
-#         if form2.is_valid():
-#             form2.save()
-#         return super().post(request, *args, **kwargs)
 
 
 class ProfileUpdateView(LoginRequiredMixin, DataMixin, UpdateView):
@@ -126,14 +87,12 @@ class ProfileUpdateView(LoginRequiredMixin, DataMixin, UpdateView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ProfileUpdateView, self).get_context_data(**kwargs)
         c_def = self.get_user_context(title='Редагування профілю')
-        return dict(list(context.items()) + list(c_def.items()))
+        return {**context, **c_def}
 
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        form = self.form_class(request.POST, instance=request.user.profile)
-        print(form.fields)
-
-        return super().post(request, *args, **kwargs)
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset().filter(
+            pk=self.request.user.pk
+        )
 
 
 class UserDeleteView(LoginRequiredMixin, DataMixin, DeleteView):
@@ -143,7 +102,7 @@ class UserDeleteView(LoginRequiredMixin, DataMixin, DeleteView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(UserDeleteView, self).get_context_data(**kwargs)
         c_def = self.get_user_context(title=f"Видалення профілю {context['user']}")
-        return dict(list(context.items()) + list(c_def.items()))
+        return {**context, **c_def}
 
     def get_queryset(self, *args, **kwargs):
         return super().get_queryset().filter(
@@ -158,7 +117,7 @@ class UserRegisterView(NotLoggedAllow, DataMixin, CreateView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(UserRegisterView, self).get_context_data(**kwargs)
         c_def = self.get_user_context(title='Створити користувача')
-        return dict(list(context.items()) + list(c_def.items()))
+        return {**context, **c_def}
 
     def form_valid(self, form):
         form.save()
