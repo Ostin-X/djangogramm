a79d4f2838d746311a962509399c244ecbe6e4f2
foxstudent102416 <andrewpeshekhonov@gmail.com>
2022-09-15 21:00:06 +0300

    image update delete page

--- posts/views.py
+++ posts/views.py
@@ -17,7 +17,7 @@ class LoginCustomView(DataMixin, LoginView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(LoginCustomView, self).get_context_data(**kwargs)
-        c_def = self.get_user_context(title='Користувачі')
+        c_def = self.get_user_context(title='Логін')
         return {**context, **c_def}
 
 
