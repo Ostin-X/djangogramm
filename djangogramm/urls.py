3d1aa8add408f5a01aa327520621a4d31049852d
foxstudent102416 <andrewpeshekhonov@gmail.com>
2022-09-15 20:06:52 +0300

    image update delete page

--- djangogramm/urls.py
+++ djangogramm/urls.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth import login
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,9 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('posts.urls')),
-                  # # path('/users/login', login, name='login'),
                   path('users/', include('django.contrib.auth.urls')),
-                  # path('users/', include('users.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # def pageNotFound(request, exception):
