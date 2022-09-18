5c4da68adcc3eacbd59abd8e703e1515b32f4397
foxstudent102416 <andrewpeshekhonov@gmail.com>
2022-09-18 12:30:36 +0300

    url and client tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,5 @@
+import json
+
 from django.test import TestCase, Client
 
 import pytest
@@ -37,11 +39,12 @@ class PostViewsTestCase(TestCase):
         # assert response.__dict__ == 'posts/post_list.html2'
 
         self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual((response.context_data['object_list']), (Post.objects.order_by('-date').all()))
         self.assertTemplateUsed(response, 'posts/post_list.html')
-        self.assertTemplateNotUsed(response, 'posts/post_list.html2')
+        self.assertTemplateNotUsed(response, 'posts/post_detail.html')
         self.assertContains(response, 'Мій тестовий тайтл')
         self.assertContains(response,
-                            'Дуже багато тексту 123456789012345678901234567890123456789012345678901234567890123456789012345678901')
+                            'Дуже багато тексту ' + '1234567890' * 8 + '1')
 
     def test_post_detail_GET_loggedin(self):
         # self.client.force_login(User.objects.get_or_create(username='testuser')[0])
@@ -49,27 +52,66 @@ class PostViewsTestCase(TestCase):
         response = self.client.get(reverse('post_detail', kwargs={'pk': self.post_ostin.pk}))
 
         # assert response.__dict__ == 'posts/post_list.html2'
+        # assert response.context_data['object'] == self.post_ostin.__dict__
 
         self.assertEqual(200, response.status_code)
+        self.assertEqual(response.context_data['object'], self.post_ostin)
         self.assertTemplateUsed(response, 'posts/post_detail.html')
         self.assertContains(response, 'Додати зображення')
 
-        self.client.logout()
+    def test_post_create_POST(self):
+        self.client.force_login(self.user_ostin)
+        post_query_count = Post.objects.count()
+        response = self.client.post(reverse('post_create'), {'title': 'New title'})
+
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(post_query_count + 1, Post.objects.count())
+        self.assertEqual(Post.objects.get(title='New title'), Post.objects.last())
+        self.assertEqual(Post.objects.get(title='New title').user, self.user_ostin)
+        self.assertIn(Post.objects.get(title='New title'), Post.objects.all())
+
+    def test_post_update_POST(self):
+        self.client.force_login(self.user_ostin)
+        post_query_count = Post.objects.count()
+        self.assertIn(Post.objects.get(title='Мій тестовий тайтл'), Post.objects.all())
+        self.assertEqual('Мій тестовий тайтл', Post.objects.get(pk=self.post_ostin.pk).title)
+        self.assertFalse(Post.objects.filter(title='Новий змінений тайтл').exists())
+
+        response = self.client.post(
+            reverse('post_update', kwargs={'pk': self.post_ostin.pk}), {'title': 'Новий змінений тайтл'})
+
+        self.assertEqual(302, response.status_code)
+        self.assertIn(Post.objects.get(title='Новий змінений тайтл'), Post.objects.all())
+        self.assertFalse(Post.objects.filter(title='Мій тестовий тайтл').exists())
+        self.assertEqual('Новий змінений тайтл', Post.objects.get(pk=self.post_ostin.pk).title)
+        self.assertEqual(post_query_count, Post.objects.count())
+
+    def test_post_delete_DELETE(self):
+        self.client.force_login(self.user_ostin)
+        post_query_count = Post.objects.count()
+        post_to_be_deleted = Post.objects.get(title='Мій тестовий тайтл')
+
+        self.assertIn(post_to_be_deleted, Post.objects.all())
+
+        response = self.client.delete(reverse('post_delete', kwargs={'pk': post_to_be_deleted.pk}))
+
+        self.assertEqual(302, response.status_code)
+        self.assertNotIn(post_to_be_deleted, Post.objects.all())
+        self.assertEqual(post_query_count - 1, Post.objects.count())
 
     def test_post_detail_GET_other_loggedin(self):
         self.client.force_login(self.user_ostin)
         response = self.client.get(reverse('post_detail', kwargs={'pk': Post.objects.first().pk}))
 
         self.assertEqual(200, response.status_code)
+        self.assertNotEqual(response.context_data['object'], self.post_ostin)
         self.assertTemplateUsed(response, 'posts/post_detail.html')
         self.assertNotContains(response, 'Додати зображення')
 
-        self.client.logout()
-
     def test_post_detail_GET_loggedout(self):
-        self.client.logout()
-        response = self.client.get(reverse('post_detail', kwargs={'pk': Post.objects.first().pk}))
+        post_object = Post.objects.first()
+        response = self.client.get(reverse('post_detail', kwargs={'pk': post_object.pk}))
 
         self.assertEqual(302, response.status_code)
+        self.assertEqual(response.headers['Location'], f'/users/login/?next=/posts/{post_object.pk}/')
         self.assertTemplateNotUsed(response, 'posts/post_detail.html')
-
