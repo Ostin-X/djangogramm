004bfbbbafbeac2d3bbf63f1002c9c3c121dc2e3
foxstudent102416 <andrewpeshekhonov@gmail.com>
2022-09-27 15:13:41 +0300

    django-admin commands

--- posts/management/commands/create_db.py
+++ posts/management/commands/create_db.py
@@ -5,8 +5,6 @@ from posts.views_create_db import create_all_db
 
 
 class Command(BaseCommand):
-    # def add_arguments(self, parser):
-    #     parser.add_argument('first')
     def handle(self, *args, **options):
         print('--------Command executed--------')
         print(create_all_db(request).content.decode("utf-8"))
