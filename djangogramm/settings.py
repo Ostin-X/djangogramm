1460f27e390b545adf7e32b8fff3e778ff6a8fbb
foxstudent102416 <andrewpeshekhonov@gmail.com>
2023-03-15 15:57:18 +0200

    mistakes 2

--- djangogramm/settings.py
+++ djangogramm/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'http://djangogramm.eu-north-1.elasticbeanstalk.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'djangogramm.eu-north-1.elasticbeanstalk.com']
 
 # Application definition
 
